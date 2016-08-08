892d7ac911be60115f5095e7cdc53166a92bed8b
ummis <swathi.ummineni@campsystems.com>
2016-08-08 09:47:35 +0530

    first commit

---
+++ AugChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AugChallenge
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public string CheckSumOfElements(int[] numbers)
+        {
+            if (numbers.Length <= 2)
+            {
+                return "Test Failed";
+            }
+
+            var index = 1;
+            var rightSideSum = numbers[0];
+            var leftSideSum = 0;
+
+            for (var j = index + 1; j < numbers.Length; j++)
+            {
+                leftSideSum = leftSideSum + numbers[j];
+            }
+            if (rightSideSum == leftSideSum)
+            {
+                return "TEST PASSED";
+            }
+
+            for (var k = index; k < numbers.Length - 1; k++)
+            {
+                index++;
+                rightSideSum = rightSideSum + numbers[k];
+                leftSideSum = leftSideSum - numbers[k + 1];
+
+                if (rightSideSum == leftSideSum)
+                {
+                    return "TEST PASSED";
+                }
+                
+            }
+
+            return "Test Failed";
+        }
+    }
+}
---
+++ AugChallengeTest/checkElementsTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AugChallenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AugChallengeTest
+{
+    [TestClass]
+    public class checkElementsTests
+    {
+        [TestMethod]
+        public void ShouldReturnSuccess()
+        {
+            var prgm = new Program();
+
+            var st = prgm.CheckSumOfElements(new[] { 10, 20, 5, 18, 30, 5 });
+            Assert.AreEqual(@"TEST PASSED", st);
+        }
+
+        [TestMethod]
+        public void ShouldReturnFail()
+        {
+            var prgm = new Program();
+
+            var returnstmt = prgm.CheckSumOfElements(new[] { 4, 2, 3 });
+            Assert.AreEqual("Test Failed", returnstmt);
+        }
+    }
+}
